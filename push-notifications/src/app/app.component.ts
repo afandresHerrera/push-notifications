cd9db1534df1e7b62d986aa12980000cea421948
afandresHerrera <afandres19@gmail.com>
2019-04-04 20:27:20 -0500

    registro pg

--- push-notifications/src/app/app.component.ts
+++ push-notifications/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'push-notifications';
+  public email = ""
+  public password: string = ""
+  public repeatPassword: string = ""
+
+  constructor(private notification: FlashMessagesService) {
+
+  }
+  ngOnInit() {
+    // this.notification.show('We are in about component!', { cssClass: 'alert-danger', timeout: 4000 });
+    // this.notification.grayOut(true);
+    // this.notification.grayOut(false);
+  }
+  validate() {
+    this.notification.show('Te has registrado exitosamente !!!', { cssClass: 'alert-success', timeout: 4000 });
+    console.log(this.email);
+    console.log(this.password);
+    console.log(this.repeatPassword);
+
+  }
 }
--- push-notifications/src/app/app.module.ts
+++ push-notifications/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlashMessagesModule } from 'angular2-flash-messages';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlashMessagesModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

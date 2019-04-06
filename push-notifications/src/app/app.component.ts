1fcce90a7c9a7af70bb3d5da5054599a06b24e67
afandresHerrera <afandres19@gmail.com>
2019-04-06 12:14:18 -0500

    home y login

--- push-notifications/src/app/app.component.ts
+++ push-notifications/src/app/app.component.ts
@@ -12,6 +12,10 @@ export class AppComponent implements OnInit {
   public email = ""
   public password: string = ""
   public repeatPassword: string = ""
+  public viewLogin: boolean = false
+  public viewHome: boolean = false
+  public emailLogin = ""
+  public passwordLogin = ""
 
   constructor(private notification: FlashMessagesService) {
 
@@ -22,10 +26,48 @@ export class AppComponent implements OnInit {
     // this.notification.grayOut(false);
   }
   validate() {
-    this.notification.show('Te has registrado exitosamente !!!', { cssClass: 'alert-success', timeout: 4000 });
+    var caracteres = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (caracteres.test(this.email)) {
+      if (this.password != '' && this.repeatPassword != '') {
+        if (this.password == this.repeatPassword) {
+          this.notification.show('Te has registrado exitosamente !!!', { cssClass: 'alert-success', timeout: 4000 });
+          setTimeout(() => {
+            this.viewLogin = true
+          }, 1000);
+        } else {
+          this.notification.show('Las contraseñas no coinciden intente nuevamente', { cssClass: 'alert-danger', timeout: 4000 });
+        }
+
+      } else {
+        this.notification.show('Por favor diligencie todos los campos', { cssClass: 'alert-danger', timeout: 4000 });
+      }
+    } else {
+      this.notification.show('Por favor ingrese un correo valido', { cssClass: 'alert-danger', timeout: 4000 });
+    }
     console.log(this.email);
     console.log(this.password);
     console.log(this.repeatPassword);
 
+  }
+  login() {
+    var caracteres = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (caracteres.test(this.emailLogin)) {
+      if (this.password != '') {
+        if (this.emailLogin == this.email && this.passwordLogin == this.password) {
+          this.notification.show('Te damos la bienvenida a TELEPERFORMANCE !!!', { cssClass: 'alert-success', timeout: 4000 });
+          setTimeout(() => {
+            this.viewLogin = false
+            this.viewHome = true
+          }, 1000);
+        } else {
+          this.notification.show('Usuario o contraseña invalidos por favor intente nuevamente. ', { cssClass: 'alert-danger', timeout: 4000 });
+        }
+
+      } else {
+        this.notification.show('Por favor diligencie todos los campos', { cssClass: 'alert-danger', timeout: 4000 });
+      }
+    } else {
+      this.notification.show('Por favor ingrese un correo valido', { cssClass: 'alert-danger', timeout: 4000 });
+    }
   }
 }
